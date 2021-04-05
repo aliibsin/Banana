f39028466d51ce5bb83e70ca0b3ed5a0c6cc651e
Ali Ibsin <ibsin.a1996@gmail.com>
2021-04-05 12:55:51 -0700

    finish setup

---
+++ Banana/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { env } = require('process');
+
+module.exports = {
+  entry: './frontend/banana.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
