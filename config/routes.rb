c0c05558305a1c305ff5b37a45596a3f77733842
Ali Ibsin <ibsin.a1996@gmail.com>
2021-04-08 01:25:47 -0700

    add seeds

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show, :edit] 
     resource :session, only: [:create, :destroy]
+
+    resources :projects, only: [:create, :index, :show, :edit, :destroy] 
   end
 
   root to: "static_pages#root"
